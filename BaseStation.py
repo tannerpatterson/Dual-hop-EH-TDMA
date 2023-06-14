5a1045d47fac8d086451458a1a380174465fb432
Robert Hayek <hayekr2@gmail.com>
2023-06-14 03:03:28 -0400

    Add comment describing base station code

---
+++ BaseStation.py
@@ -0,0 +1,14 @@
+'''
+Multi-Hop TDMA BASE STATION (Energy Harvesting Simulation) - T.J Smull 
+College of Engineering, Ohio Northern University
+Author: Tanner Patterson, Robert J. Hayek, Dr. Ahmed Ammar
+Date: 06/14/2023
+Copyright (c) 2023, Ohio Northern University, All rights reserved.
+'''
+
+# Base Station FSM Implemented in Python to allow for data saving.
+# Should be able to perform all base station duties and export data for 
+#analysis.
+
+if __name__ == '__main__':
+    print("BASE STATION")
