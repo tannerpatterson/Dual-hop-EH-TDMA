b4494c775f3b21ccb8efcb7baeaece42a423e6c0
Tanner Patterson <t-patterson.4@onu.edu>
2023-07-13 12:31:30 -0400

    Slight changes. Algorithm change coming soon

--- BaseStation/BaseStation.ino
+++ BaseStation/BaseStation.ino
@@ -5,22 +5,26 @@ Date: 07/06/2023
 Copyright (c) 2023, Ohio Northern University, All rights reserved.
 */
 
+#define LED 8
+
+/* GLOBALS */
 const int NETWORK_NUMBER_OF_NODES = 5; // Number of nodes on the network
 const int TIME_SLOT = 2000; // In milliseconds (ms) 10^-3
-const int THRESHOLD = 0;
-const int TIME_OUT = 5 // Number of phases till timeout
+const int THRESHOLD = 0; // In milliseconds Threshold for overlap
+const int TIME_OUT = 5; // Number of phases till timeout
 
-
-unsigned long CurrentReceiveTime = 0;
-unsigned long PreviousReceiveTime = 0;
+/* Timers */
+unsigned long CurrentTime = 0;
+unsigned long PreviousTime = 0;
 
 // Allows for a software reset, like the `RED` button, Easy one-liner
 void(* softwareReset) (void) = 0; //declare reset function @ address 0
 
 // Function that checks for overlap in packets
 bool clusterTransmissionError (unsigned long Current, unsigned long Previous) {
-   TimeDif = (Current - Previous).toInt();
-  if(TimeDif <= TIME_SLOT - THRESHOLD){
+  unsigned long TimeDif = (Current - Previous);
+  unsigned long Overlap = (unsigned long) (TIME_SLOT - THRESHOLD);
+  if(TimeDif <= Overlap){
     return true;
   }
   else{
@@ -33,42 +37,38 @@ bool packetTransmissionError ( ){
   // TODO Write this
 }
 
-// Function that checks for a dead network
-bool timeOut(unsigned long Previous){
-  unsigned long TimeOutCheck = TIME_OUT*TIME_SLOT*NETWORK_NUMBER_OF_NODES*millies();
-  if(Previous > TimeOutCheck){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-
-
 
 void basestationFSM() {
   static enum { SYNC, ACTIVE, RESYNC } state = SYNC;
 
   switch (state) {
     case SYNC:
+      CurrentTime = millis();
       Serial.println("S00");
       state = ACTIVE;
       break;
 
     case ACTIVE:
       // Check for timeout
-      timeOut(PreviousReceiveTime);
-      if(Serial.available() > 0) {  
-        // Check for overlap in packets
-        PreviousReceiveTime = CurrentReceiveTime;
-        CurrentReceiveTime = millis();
-        if(clusterTransmissionError(CurrentReceiveTime,PreviousReceiveTime)){
-          state = RESYNC;
-        }
-        else{
-        // TODO: Recieve bulk packet and seperate it
+      if(millis() >= TIME_OUT* TIME_SLOT* NETWORK_NUMBER_OF_NODES* CurrentTime){
+        state = SYNC;
+      }
+
+      // Check for incoming packets 
+      else{
+        if(Serial.available() > 0) {  
+          // Check for bulk packet overlap
+          PreviousTime = CurrentTime;
+          CurrentTime = millis();
+          if(clusterTransmissionError(CurrentTime,PreviousTime)){
+            state = RESYNC;
+          }
+
+          // Seperate bulk packet and check for errors
+          else{
+          // TODO: Recieve bulk packet and seperate it
           
-        }    
+          }    
       } 
 
       break;
@@ -78,11 +78,11 @@ void basestationFSM() {
       break;
 
     default:
-      state = DEAD;
+      state = SYNC;
       break;
   }
-
-
+  }
+}
 
 void setup() {
   Serial.begin(9600); // Baud 9600
