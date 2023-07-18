a548fe832d978147ada2c69c22d5d3a8820b8e70
Tanner Patterson <t-patterson.4@onu.edu>
2023-07-18 11:25:55 -0400

    Working on Clusterhead overlap - work in progress

--- BaseStation/BaseStation.ino
+++ BaseStation/BaseStation.ino
@@ -11,8 +11,10 @@ Copyright (c) 2023, Ohio Northern University, All rights reserved.
 const int NETWORK_NUMBER_OF_NODES = 5; // Number of nodes on the network
 const int CLUSTERS =2; // Number of clusters on the network
 const int TIME_SLOT = 2000; // In milliseconds (ms) 10^-3
-const int THRESHOLD = 0; // In milliseconds Threshold for overlap
+const int THRESHOLD = 0; // In milliseconds threshold for overlap
 const int TIME_OUT = 5; // Number of phases till timeout
+cont int ERROR = 0; // In milliseconds time for transmission error
+const int CLUSTERHEADS [CLUSTERS] ={}; // Array full of custer head IDS
 
 /* Timers */
 unsigned long CurrentTime = 0;
@@ -22,11 +24,15 @@ unsigned long PreviousTime = 0;
 int OutOfEnergyCount = 0;
 
 /* Arrays */
-int ClusterHeadIDs [CLUSTERS] ={};
 unsigned long LastRecievedTime [CLUSTERS] = {};
 
-/* Flags */
-bool FullArray = false;
+/* Fancy Custer Head Stuff */
+String packet ="";
+String incomingString ="";
+String SyncCheck = "";
+int IdRecieved = 0;
+int ClusterNumberReceived= 0;
+int ClusterHeadCheck = 0;
 
 // Allows for a software reset, like the `RED` button, Easy one-liner
 void(* softwareReset) (void) = 0; //declare reset function @ address 0
@@ -57,36 +63,52 @@ void basestationFSM() {
   switch (state) {
     case START:
       CurrentTime = millis();
-      Serial.println("00S");
+      Serial.println("00S0");
       state = ACTIVE;
       break;
 
     case ACTIVE:
       // Check for timeout
-      if(LastRecievedTime[OutOfEnergyCount] <= millis()-(TIME_OUT* TIME_SLOT* NETWORK_NUMBER_OF_NODES) && FullArray){
-        int GlobalID = ClusterHeadIDs[OutOfEnergyCount];
+      if(LastRecievedTime[OutOfEnergyCount] <= millis()-(TIME_OUT* TIME_SLOT* NETWORK_NUMBER_OF_NODES)-ERROR){
+        int GlobalID = CLUSTERHEADS[OutOfEnergyCount];
         int ClusterID = OutOfEnergyCount + 1;
-        String packet = GlobalID+ClusterID+"R";
-        Serial.println("packet");
+        String packet = GlobalID+ClusterID+"T0";
+        Serial.println(packet);
       }
 
       // Check for incoming packets 
       else{
         if(Serial.available() > 0) {  
-
-          // Check for Cluster Head overlap
-          PreviousTime = CurrentTime;
-          CurrentTime = millis();
-          if(clusterTransmissionError(CurrentTime,PreviousTime)){
-            state = RESTART;
-          }
-
-          // Seperate bulk packet and check for errors
-          else{
-            
-          // TODO: Recieve bulk packet and seperate it
+          //Check to make sure it is a CH packet
+          incomingString = Serial.readStringUntil('\r');
+          IdRecieved = incomingString.substring(0,1).toInt();
+          ClusterHeadCheck = incomingString.substring(2,3).toInt();
+          if(ClusterHeadCheck == 1){
           
-          }    
+            // Check for Cluster Head overlap (Might get deleted)
+            PreviousTime = CurrentTime;
+            CurrentTime = millis();
+            if(clusterTransmissionError(CurrentTime,PreviousTime)){
+              for(int index = 0; index < NETWORK_NUMBER_OF_NODES; index++){
+                // Send Overlap Message
+                if(IdRecieved == CLUSTERHEADS[index]){
+                  int Cluster1 = index+1;
+                  int Cluster2 = ((index-1)%CLUSTERS)+1;
+                  String packet = IdRecieved+Cluster1+"H"+Cluster2;
+                  Serial.println(packet);
+                }
+              }
+            }
+
+            //Fill Time Array
+
+            // Seperate bulk packet
+            else{            
+            
+            
+          
+            }  
+          }  
       } 
 
       Serial.flush();
